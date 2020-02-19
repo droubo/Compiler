1156352678591d222d7cff2effd01cd85d9108d7
NicolaiGoon <xgounakis5@gmail.com>
2020-02-19 22:29:24 +0200

    first

---
+++ Phase1/al.c
@@ -0,0 +1,26 @@
+/**
+ *  al.c
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+
+int main(int argc , char** args)
+{
+    if(argc == 1)
+    {
+       /* output --> stdout */
+    }
+    else if(argc == 2)
+    {
+         /* output --> args[1] */
+         int _t;
+       
+    }
+
+}
+
+
