65322114721e31c70447a58c53525d84e8d33c9d
NicolaiGoon <xgounakis5@gmail.com>
2020-04-05 13:50:01 +0300

    added .h

--- Phase3/quads/quads.c
+++ Phase3/quads/quads.c
@@ -1,67 +1,6 @@
 /* Some code Taken from lecture 9, Slides 37, 38 
    Frontisthrio 4 Slide 15 */
-#include <stdlib.h>
-#include <stdio.h>
-#include <assert.h>
-#include <string.h>
-#include "../symtab/symtable.h"
-
-#define EXPAND_SIZE 1024
-#define CURR_SIZE (total*sizeof(quad))
-#define NEW_SIZE (EXPAND_SIZE * sizeof(quad) + CURR_SIZE)
-
-typedef enum expr_t {
-    var_e,
-    tableitem_e,
-
-    programfunc_e,
-    libraryfunc_e,
-
-    arithexpr_e,
-    boolexpr_e,
-    assignexpr_e,
-    newtable_e,
-
-    costnum_e, 
-    constbool_e,
-    conststring_e,
-
-    nil_e
-} expr_t;
-
-typedef enum iopcode {
-    assign, add, sub, 
-    mul, diva, mod, 
-    uminus, and, or, 
-    not, if_eq, if_noteq, 
-    if_lesseq, if_greatereq, if_less, 
-    if_greater, call, param, ret, 
-    getretval, funcstart, funcend, 
-    tablecreate, tablegetelem, tablesetelem
-} iopcode;
-
-typedef struct expr {
-    expr_t type;
-    SymTabEntry * sym;
-    struct expr * index;
-    double numConst;
-    char * strConst;
-    unsigned char boolConst;
-    struct expr * next;
-} expr;
-
-typedef struct quad {
-    iopcode op;
-    expr * result;
-    expr * arg1;
-    expr * arg2;
-    unsigned label;
-    unsigned line;
-} quad;
-
-quad * quads = (quad *) 0;
-unsigned total = 0;
-unsigned int currQuad = 0;
+#include "quads.h"
 
 void expand (void){
     assert(total == currQuad);
@@ -74,9 +13,10 @@ void expand (void){
     total += EXPAND_SIZE;
 }
 
-expr newexpr(expr_t type, SymTabEntry sym){
+expr newexpr(expr_t type, SymTabEntry* sym){
     expr temp;
     temp.type = type;
+    temp.sym = sym;
     return temp;
 }
 
@@ -90,8 +30,4 @@ void emit (iopcode op, expr * arg1, expr * arg2, expr * result, unsigned label, 
     p->result = result;
     p->label = label;
     p->line = line;
-}
-
-int main(){
-    printf("poop\n");
 }
---
+++ Phase3/quads/quads.h
@@ -0,0 +1,75 @@
+/* Some code Taken from lecture 9, Slides 37, 38 
+   Frontisthrio 4 Slide 15 */
+#ifndef _QUADS_H
+#define _QUADS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "../symtab/symtable.h"
+
+#define EXPAND_SIZE 1024
+#define CURR_SIZE (total*sizeof(quad))
+#define NEW_SIZE (EXPAND_SIZE * sizeof(quad) + CURR_SIZE)
+
+typedef enum expr_t {
+    var_e,
+    tableitem_e,
+
+    programfunc_e,
+    libraryfunc_e,
+
+    arithexpr_e,
+    boolexpr_e,
+    assignexpr_e,
+    newtable_e,
+
+    costnum_e, 
+    constbool_e,
+    conststring_e,
+
+    nil_e
+} expr_t;
+
+typedef enum iopcode {
+    assign, add, sub, 
+    mul, diva, mod, 
+    uminus, and, or, 
+    not, if_eq, if_noteq, 
+    if_lesseq, if_greatereq, if_less, 
+    if_greater, call, param, ret, 
+    getretval, funcstart, funcend, 
+    tablecreate, tablegetelem, tablesetelem
+} iopcode;
+
+typedef struct expr {
+    expr_t type;
+    SymTabEntry * sym;
+    struct expr * index;
+    double numConst;
+    char * strConst;
+    unsigned char boolConst;
+    struct expr * next;
+} expr;
+
+typedef struct quad {
+    iopcode op;
+    expr * result;
+    expr * arg1;
+    expr * arg2;
+    unsigned label;
+    unsigned line;
+} quad;
+
+quad * quads = (quad *) 0;
+unsigned total = 0;
+unsigned int currQuad = 0;
+
+void expand (void);
+
+expr newexpr(expr_t type, SymTabEntry* sym);
+
+void emit (iopcode op, expr * arg1, expr * arg2, expr * result, unsigned label, unsigned line);
+
+#endif
