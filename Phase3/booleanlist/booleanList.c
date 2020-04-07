60c7e8b4431115d4a48f7132e8ec16c2a8693f61
George Kokiadis <georgekoki@users.noreply.github.com>
2020-04-07 22:05:53 +0300

    tiny fix k bye

--- Phase3/booleanlist/booleanList.c
+++ Phase3/booleanlist/booleanList.c
@@ -1,5 +1,6 @@
 #include "booleanList.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 #ifndef NULL
 #define NULL 0
@@ -24,7 +25,7 @@ booleanList * booleanList_merge(booleanList * list1, booleanList * list2){
     list = list1;
     currNode = list;
 
-    while(currNode != NULL){
+    while(currNode->next != NULL){
         currNode = currNode->next;
     }
 
@@ -33,6 +34,15 @@ booleanList * booleanList_merge(booleanList * list1, booleanList * list2){
     return list;
 }
 
-int main(){
+int test(){
+    booleanList * list1 = booleanList_makelist(1);
+    booleanList * list2 = booleanList_makelist(2);
+    booleanList * list3 = booleanList_makelist(3);
+    booleanList * list4 = booleanList_makelist(4);
+    booleanList * list = booleanList_merge(booleanList_merge(list1, list2), booleanList_merge(list3, list4));
 
+    while(list != NULL){
+        printf("%d\n", list->label);
+        list = list->next;
+    }
 }
