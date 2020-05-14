f0a4da3cb40f0afe17044cdb4e83a090f1f26ece
NicolaiGoon <xgounakis5@gmail.com>
2020-05-14 18:39:06 +0300

    added content

--- Phase4/target code/target.c
+++ Phase4/target code/target.c
@@ -1,3 +1,69 @@
 #include "target.h"
 
+void make_operand(expr* e, vmarg* arg){
+
+    switch(e->type) {
+        case var_e:
+        case tableitem_e:
+        case arithexpr_e:
+        case boolexpr_e:
+        case newtable_e: {
+            arg->val = e->sym->offset;
+            switch(e->sym->space){
+                case programvar: arg->type = global_a; break;
+                case functionlocal: arg->type = local_a; break;
+                case formalarg: arg->type = formal_a; break;
+                default: assert(0);
+            }
+        }
+        
+        case constbool_e: {
+            arg->val = e->boolConst;
+            arg->type = bool_a; break;
+        }
+
+
+        case conststring_e: {
+            arg->val = const_newstring(e->strConst);
+            arg->type = string_a; break;
+        }
+
+        case constnum_e: {
+            arg->val = consts_newnumber(e->numConst);
+            arg->type = number_a; break;
+        }
+
+        case nil_e: arg->type = nil_a; break;
+
+        case programfunc_e: {
+            arg->type = userfunc_a;
+            arg->val = e->sym->taddress;
+            break;
+        }
+
+        case libraryfunc_e: {
+            arg->type = libfunc_a;
+            arg->val = libfuncs_newused(e->sym->name);
+            break;
+        }
+        
+        default: assert(0);
+
+    }
+}
+
+/*
+
+patch_incomplete_jumps() 
+{
+    for each incomplete jump x do 
+    {
+        if x.iaddress = intermediate code size then
+            instructions[x.instrNo].result = target code size;
+        else
+            instructions[x.instrNo].result = quads[x.iaddress].taddress;
+    }
+}
+
+*/
 
--- Phase4/target code/target.h
+++ Phase4/target code/target.h
@@ -1,5 +1,88 @@
 #ifndef _TARGET_H
 #define _TARGET_H
 
+#include "../quads/quads.h"
+
+typedef enum vmopcode
+{
+    assign_v, add_v, sub_v, 
+    mul_v, div_v, mod_v, 
+    uminus_v, and_v, or_v, 
+    not_v, jeq_v, jne_v, 
+    jle_v, jge_v, jlt_v, 
+    jgt_v, call_v, pusharg_v,
+    funcenter_v, funcexit_v, newtable_v,
+    tablegetelem_v, tablesetelem_v, nop_v
+
+}vmopcode;
+
+typedef enum vmarg_t 
+{
+   label_a =0,
+   global_a=1,
+   formal_a =2,
+   local_a =3,
+   number_a =4,
+   string_a =5,
+   bool_a =6,
+   nil_a =7,
+   userfunc_a =8,
+   libfunc_a =9,
+   retval_a=10
+} vmarg_t;
+
+typedef struct vmarg
+{
+    vmarg_t type;
+    unsigned int val;
+}vmarg;
+
+typedef struct instruction 
+{
+    vmopcode opcode;
+    vmarg result;
+    vmarg arg1;
+    vmarg arg2;
+    unsigned int srcLine;
+};
+
+struct userfunc
+{
+    unsigned int address;
+    unsigned int localSize;
+    char* id;
+}
+
+typedef struct incomplete_jump
+{
+    unsigned int instrNo; /* jump instruction number*/
+    unsigned int iaddress;/*i-code jump target address*/
+    incomplete_jump* next;/*linked list*/
+}incomplete_jump;
+
+incomplete_jump* ij_head = (incomplete_jump*) 0;
+unsigned int ij_total = 0;
+
+/* for vm */
+/*
+double* numConsts;
+unsigned int totalNumConsts;
+
+char** stringConsts;
+unsigned int totalStringConsts;
+
+char** namedLibFuncs;
+unsigned int totalNamedLibFuncs;
+
+userfunc* userFuncs;
+unsinged int totalUserFuncs;
+*/
+
+unsigned consts_newstring(char *s);
+unsigned consts_newnumber(double n);
+unsigned libfuncs_newused(char *s);
+
+
+
 
 #endif
